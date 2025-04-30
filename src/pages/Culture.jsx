c9c6c02f9c01819eadf0a47e76e92a2d43487998
Lorie <rie_bsly@icloud.com>
2025-04-29 22:01:06 -0700

    added content to culture page

--- src/pages/Culture.jsx
+++ src/pages/Culture.jsx
@@ -1,10 +1,58 @@
 function Culture() {
   return (
     <>
-      <h1></h1>
+      <section>
+        <h1>Culture and Traditions</h1>
+        <p>
+          The Philippines is a country rich in culture and traditions shaped by
+          its diverse history, indigenous roots, and foreign influences.
+        </p>
+        <p>
+          From colorful festivals to deep-rooted family values, Filipino culture
+          reflects warmth, resilience, and a strong sense of community.
+        </p>
+      </section>
+      <section>
+        <h2>Bayanihan</h2>
+        <p>represents the strong sense of community among Filipinos.</p>
+        <div class="bayanihan-grid">
+          <img src="media/bayanihan.jpg" alt="bayanihan" />
+          <img src="media/bayanihan(2).jpg" alt="bayanihan" />
+          <img src="media/bayanihan(3).jpg" alt="bayanihan" />
+          <img src="media/bayanihan(4).jpg" alt="bayanihan" />
+        </div>
+      </section>
+      <section class="hospitality">
+        <div class="headline">
+          <p>Filipinos are known worldwide for their</p>
+          <h2>Hospitality</h2>
+        </div>
+        <div class="hospitality-grid">
+          <img src="media/hospitality.webp" alt="bayanihan" />
+          <img src="media/hospitality(2).webp" alt="bayanihan" />
+          <img src="media/hospitality(3).jpg" alt="bayanihan" />
+          <img src="media/hospitality(4).jpg" alt="bayanihan" />
+        </div>
+      </section>
+      <section class="respect">
+        <div class="headline">
+          <h2>Respect</h2>
+          <p>
+            is deeply ingrained in Filipino culture and is reflected in the way
+            people interact with elders, family members, and even strangers.
+          </p>
+        </div>
+        <div class="respect-grid">
+          <img src="media/respect.jpg" alt="bayanihan" />
+          <p>
+            Filipinos also show deep respect for elders by using 'po' and 'opo'
+            when speaking.
+          </p>
+          <img src="media/respect(2).webp" alt="bayanihan" />
+        </div>
+      </section>
     </>
   );
 }
 
 export default Culture;
-  
