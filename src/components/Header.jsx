d390b98575950f47a8c404cd49661a6fb63c9b64
Lorie <rie_bsly@icloud.com>
2025-04-29 21:40:24 -0700

    added content to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <header className="flex justify-center items-center gap-5 pt-5 md:gap-10 z-[9999]">
             <div className="line w-2/5"></div>
             <Link to="/">
-                <img className="size-25 md:size-30"src="favicon/favicon.svg" alt="logo" />
+                <img className="size-25 md:size-28"src="favicon/favicon.svg" alt="logo" />
             </Link>
             <div className="line w-2/5"></div>
         </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
 function Home() {
   return (
-    <>
-      <h1></h1>
-    </>
+    <section className="h-[80vh] flex flex-col md:flex-row items-center md:items-center justify-center md:justify-center px-6 md:px-16">
+      <div className="text-center md:w-1/2 flex flex-col justify-center h-full">
+        <h1 className="text-3xl font-bold mb-2">
+          Philippines at a Glance
+        </h1>
+        <p className="text-lg text-gray-600">
+          Archipelago of Beauty and History
+        </p>
+      </div>
+      <div className="w-full md:w-1/2">
+        <iframe
+          className="w-full aspect-video rounded-2xl shadow-lg"
+          src="https://www.youtube.com/embed/eSUmkFPln_U?autoplay=1&mute=1&controls=0&rel=0&modestbranding=1&playsinline=1&loop=1&playlist=eSUmkFPln_U"
+          title="Philippines tourism video"
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </section>
   );
 }
 
